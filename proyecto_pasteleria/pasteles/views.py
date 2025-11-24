bf1c73c39e666fd55e2d25f45855c25a5f10aaa2
SrJaimee <jaimeandujo15@gmail.com>
2025-11-24 03:04:12 -0700

    feat(views): creacion de funcion para una vista "Saludo"

--- proyecto_pasteleria/pasteles/views.py
+++ proyecto_pasteleria/pasteles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+#Creacion de una funcion para imprimir una bienvenida
+def inicio(request):
+    return HttpResponse("<h1>Bienvenido a la Pasteleria</h1>")
