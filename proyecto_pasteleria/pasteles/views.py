46c7d11864a32ade3b309ef7757a7aae9f0fa0c7
SrJaimee <jaimeandujo15@gmail.com>
2025-11-24 03:25:43 -0700

    feat(views): creacion de una funcion para mostar el documento html

--- proyecto_pasteleria/pasteles/views.py
+++ proyecto_pasteleria/pasteles/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-#Creacion de una funcion para imprimir una bienvenida
+#Creacion de una funcion para imprimir una bienvenida, imprime un texto html
 def inicio(request):
     return HttpResponse("<h1>Bienvenido a la Pasteleria</h1>")
+
+#Creacion de una funcion para mostrar el documento html
+def index(request):
+    return render(request, 'paginas/index.html')
