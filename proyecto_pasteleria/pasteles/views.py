2b0e8d13b219e8b4a5ef2234df0535a3c4483888
SrJaimee <jaimeandujo15@gmail.com>
2025-11-25 19:46:35 -0700

    feat: agregar plantillas de inicio e inventario y actualizar rutas en views.py

--- proyecto_pasteleria/pasteles/views.py
+++ proyecto_pasteleria/pasteles/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse
 
 #Creacion de una funcion para imprimir una bienvenida, imprime un texto html
 def inicio(request):
-    return render(request, 'paginas/inicio.html')
+    return render(request, 'home/inicio.html')
 
 #Creacion de una funcion para mostrar el documento html
 def index(request):
-    return render(request, 'paginas/inventario.html')
+    return render(request, 'gestionarDatos/inventario.html')
 
 #Creacion de una funcion para mostar el documento registro (Que es el index de registro)
 def gestionarDatos(request):
